ad11106e614d33d6159ef8acd89120e06eb71082
squarecircle520 <142723147+squarecircle520@users.noreply.github.com>
2025-06-03 22:35:41 +0800

    paint_fixed_surface.py

--- src/test/szr/paint_fixed_surface.py
+++ src/test/szr/paint_fixed_surface.py
@@ -62,13 +62,13 @@ class MoveItArm(object):
                 joint_trajectory.append(joint_angles)
         return joint_trajectory
 
-    def execute_joint_trajectory(self, joint_traj, tolerance=0.01, delay=0.02):
+    def execute_joint_trajectory(self, joint_traj, tolerance=0.01, delay=0.002):
         for i, joints in enumerate(joint_traj):
             self.arm_group.set_goal_joint_tolerance(tolerance)
             self.arm_group.set_joint_value_target(joints)
             success = self.arm_group.go(wait=True)
             rospy.sleep(delay)
-            rospy.loginfo("Point %d/%d completed", i+1 , len(joint_trajectory))
+            rospy.loginfo("Point %d/%d completed", i+1 , len(joint_traj))
             if not success:
                 rospy.logwarn("Failed to reach joint configuration: %s", str(joints))
                 return False
